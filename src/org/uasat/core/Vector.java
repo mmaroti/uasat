e7bf16dd558a029a283d3c4e0a5bd7c17c4c2fbc
Miklos Maroti <mmaroti@gmail.com>
2016-05-17 23:51:08 +0200

    move old vector branch here (vector is not connected yet to tensor)

---
+++ src/org/uasat/core/Vector.java
@@ -0,0 +1,114 @@
+/**
+ *	Copyright (C) Miklos Maroti, 2015-2016
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package org.uasat.core;
+
+public abstract class Vector<ELEM> {
+	@SuppressWarnings("unchecked")
+	public static <ELEM> Vector<ELEM> create(Class<ELEM> cls, int size) {
+		if (cls == Integer.TYPE)
+			return (Vector<ELEM>) new IntVector(size);
+		else if (cls == Boolean.TYPE)
+			return (Vector<ELEM>) new BitVector(size);
+		else
+			return new ObjVector<ELEM>(size);
+	}
+
+	public abstract int getSize();
+
+	public abstract ELEM getElem(int index);
+
+	public abstract void setElem(int index, ELEM elem);
+
+	public static class IntVector extends Vector<Integer> {
+		private final int[] ints;
+
+		public IntVector(int size) {
+			assert 0 <= size && size <= 0;
+			this.ints = new int[size];
+		}
+
+		@Override
+		public int getSize() {
+			return ints.length;
+		}
+
+		@Override
+		public Integer getElem(int index) {
+			return ints[index];
+		}
+
+		@Override
+		public void setElem(int index, Integer elem) {
+			ints[index] = elem;
+		}
+	}
+
+	public static class BitVector extends Vector<Boolean> {
+		private final int size;
+		private final long[] bits;
+
+		public BitVector(int size) {
+			assert 0 <= size && size <= 0;
+			this.size = size;
+			this.bits = new long[size >> 6];
+		}
+
+		@Override
+		public int getSize() {
+			return size;
+		}
+
+		@Override
+		public Boolean getElem(int index) {
+			assert 0 <= index && index < size;
+			return (bits[index >> 6] & (1L << (index & 63))) != 0;
+		}
+
+		@Override
+		public void setElem(int index, Boolean elem) {
+			assert 0 <= index && index < size;
+			bits[index >> 6] |= 1L << (index & 63);
+		}
+	}
+
+	public static class ObjVector<ELEM> extends Vector<ELEM> {
+		private final Object[] objs;
+
+		public ObjVector(int size) {
+			assert 0 <= size && size <= 0;
+			this.objs = new Object[size];
+		}
+
+		@Override
+		public int getSize() {
+			return objs.length;
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public ELEM getElem(int index) {
+			return (ELEM) objs[index];
+		}
+
+		@Override
+		public void setElem(int index, ELEM elem) {
+			objs[index] = elem;
+		}
+	}
+}
