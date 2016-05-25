9a62ab87892bb16ee7ce0634750e6ea404e0bbac
Miklos Maroti <mmaroti@gmail.com>
2016-05-25 23:36:06 +0200

    thinking of the fano plane

--- src/org/uasat/research/FanoPlane2.java
+++ src/org/uasat/research/FanoPlane2.java
@@ -23,55 +23,104 @@ import java.util.*;
 import org.uasat.core.*;
 
 public class FanoPlane2 {
-	public static class Planes extends SatProblem {
-		public Tensor<Boolean> pts;
+	public static final SatSolver<?> SOLVER = SatSolver.getDefault();
 
-		public Planes() {
-			super(new int[] { 7, 3 });
+	private static final Tensor<Boolean> GEN2 = Tensor.matrix(2, 3,
+			Arrays.asList(true, false, false, true, true, true));
 
-			pts = Tensor.matrix(3, 7, Arrays.asList(true, false, false, false,
-					true, false, false, false, true, true, true, false, true,
-					false, true, false, true, true, true, true, true));
-		}
+	public static <BOOL> Tensor<BOOL> generate2(BoolAlgebra<BOOL> alg,
+			Tensor<BOOL> tensor) {
+		assert tensor.getOrder() == 2 && tensor.getDim(0) == 7
+				&& tensor.getDim(1) == 2;
 
-		@Override
-		public <ELEM> ELEM compute(BoolAlgebra<ELEM> alg,
-				List<Tensor<ELEM>> tensors) {
-			Tensor<ELEM> g = tensors.get(0);
-			Tensor<ELEM> p = Tensor.map(alg.type, alg.LIFT, pts);
+		Contract<BOOL> c = Contract.linear(alg);
+		c.add(tensor, "ij");
+		c.add(Tensor.map(alg.type, alg.LIFT, GEN2), "jk");
+		return c.get("ik");
+	}
 
-			Contract<ELEM> c = Contract.linear(alg);
-			c.add(g, "ij");
-			c.add(p, "jk");
-			Tensor<ELEM> t = c.get("ik");
-			ELEM b = Tensor.fold(alg.ALL, 1, Tensor.fold(alg.ANY, 1, t)).get();
+	public static <BOOL> BOOL isMinimal2(BoolAlgebra<BOOL> alg,
+			Tensor<BOOL> tensor) {
+		assert tensor.getOrder() == 2 && tensor.getDim(0) == 7
+				&& tensor.getDim(1) == 3;
 
-			List<Tensor<ELEM>> xs = Tensor.unstack(t);
+		List<Tensor<BOOL>> ts = Tensor.unstack(tensor);
+		BOOL b = Tensor.fold(alg.ANY, 1, ts.get(0)).get();
+		b = alg.and(b, alg.lexLess(ts.get(0), ts.get(1)));
+		b = alg.and(b, alg.lexLess(ts.get(1), ts.get(2)));
 
-			b = alg.and(b, alg.lexLess(xs.get(0), xs.get(1)));
-			b = alg.and(b, alg.lexLess(xs.get(0), xs.get(2)));
-			b = alg.and(b, alg.lexLess(xs.get(0), xs.get(3)));
-			b = alg.and(b, alg.lexLess(xs.get(0), xs.get(4)));
-			b = alg.and(b, alg.lexLess(xs.get(0), xs.get(5)));
-			b = alg.and(b, alg.lexLess(xs.get(0), xs.get(6)));
+		return b;
+	}
 
-			b = alg.and(b, alg.lexLess(xs.get(1), xs.get(2)));
-			b = alg.and(b, alg.lexLess(xs.get(1), xs.get(3)));
-			b = alg.and(b, alg.lexLess(xs.get(1), xs.get(4)));
-			b = alg.and(b, alg.lexLess(xs.get(1), xs.get(5)));
-			b = alg.and(b, alg.lexLess(xs.get(1), xs.get(6)));
+	public static Tensor<Boolean> subspaces2() {
+		SatProblem problem = new SatProblem(new int[] { 7, 2 }) {
+			@Override
+			public <BOOL> BOOL compute(BoolAlgebra<BOOL> alg,
+					List<Tensor<BOOL>> tensors) {
+				Tensor<BOOL> tensor = tensors.get(0);
+				tensor = generate2(alg, tensor);
+				return isMinimal2(alg, tensor);
+			}
+		};
 
-			b = alg.and(b, alg.lexLess(xs.get(2), xs.get(4)));
-			b = alg.and(b, alg.lexLess(xs.get(2), xs.get(5)));
-			b = alg.and(b, alg.lexLess(xs.get(2), xs.get(6)));
+		Tensor<Boolean> ss = problem.solveAll(SOLVER).get(0);
+		System.out.println("subspaces of dim 2: " + ss.getLastDim());
 
-			return b;
-		}
+		return ss;
+	}
+
+	private static final Tensor<Boolean> GEN3 = Tensor.matrix(3, 7, Arrays
+			.asList(true, false, false, false, true, false, false, false, true,
+					true, true, false, true, false, true, false, true, true,
+					true, true, true));
+
+	public static <BOOL> Tensor<BOOL> generate3(BoolAlgebra<BOOL> alg,
+			Tensor<BOOL> tensor) {
+		assert tensor.getOrder() == 2 && tensor.getDim(0) == 7
+				&& tensor.getDim(1) == 3;
+
+		Contract<BOOL> c = Contract.linear(alg);
+		c.add(tensor, "ij");
+		c.add(Tensor.map(alg.type, alg.LIFT, GEN3), "jk");
+		return c.get("ik");
+	}
+
+	public static <BOOL> BOOL isMinimal3(BoolAlgebra<BOOL> alg,
+			Tensor<BOOL> tensor) {
+		assert tensor.getOrder() == 2 && tensor.getDim(0) == 7
+				&& tensor.getDim(1) == 7;
+
+		List<Tensor<BOOL>> ts = Tensor.unstack(tensor);
+		BOOL b = Tensor.fold(alg.ANY, 1, ts.get(0)).get();
+		b = alg.and(b, alg.lexLess(ts.get(0), ts.get(1)));
+		b = alg.and(b, alg.lexLess(ts.get(1), ts.get(2)));
+		b = alg.and(b, alg.lexLess(ts.get(1), ts.get(3)));
+		b = alg.and(b, alg.lexLess(ts.get(2), ts.get(4)));
+		b = alg.and(b, alg.lexLess(ts.get(2), ts.get(5)));
+		b = alg.and(b, alg.lexLess(ts.get(2), ts.get(6)));
+
+		return b;
+	}
+
+	public static Tensor<Boolean> subspaces3() {
+		SatProblem problem = new SatProblem(new int[] { 7, 3 }) {
+			@Override
+			public <BOOL> BOOL compute(BoolAlgebra<BOOL> alg,
+					List<Tensor<BOOL>> tensors) {
+				Tensor<BOOL> tensor = tensors.get(0);
+				tensor = generate3(alg, tensor);
+				return isMinimal3(alg, tensor);
+			}
+		};
+
+		Tensor<Boolean> ss = problem.solveAll(SOLVER).get(0);
+		System.out.println("subspaces of dim 3: " + ss.getLastDim());
+
+		return ss;
 	}
 
 	public static void main(String[] args) {
-		Planes p = new Planes();
-		SatSolver<?> solver = SatSolver.getDefault();
-		System.out.println(p.solveAll(solver).size());
+		subspaces2();
+		subspaces3();
 	}
 }
